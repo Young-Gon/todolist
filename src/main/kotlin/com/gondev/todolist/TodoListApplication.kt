3db3c1bdc9919de25391b54805a7fe8924f65f1a
yg1027 <yg1028@seedit.co.kr>
2019-02-25 10:57:34 +0900

    테스트 어노테이션 삭제

--- src/main/kotlin/com/gondev/todolist/TodoListApplication.kt
+++ src/main/kotlin/com/gondev/todolist/TodoListApplication.kt
@@ -1,13 +1,10 @@
 package com.gondev.todolist
 
 import com.gondev.todolist.config.AppProperties
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
-import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@ExtendWith(SpringExtension::class)
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties::class)
 class TodoListApplication
